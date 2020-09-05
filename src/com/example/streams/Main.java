bfa58616b6f643d9546f408101bcd8087935e496
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2020-09-05 15:01:58 +0200

    cd..

--- src/com/example/streams/Main.java
+++ src/com/example/streams/Main.java
@@ -3,6 +3,7 @@ package com.example.streams;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -23,10 +24,26 @@ public class Main {
 	    list.stream().map(a->a*a).filter(a->a%2==0).forEach(System.out::println);
 
 	    Main main = new Main();
+	    main.example1();
+	    main.example2();
 
     }
 
     void example1(){
+		List<Integer> list = new ArrayList<>();
+		for(int i=0;i<100;i++)
+			list.add(i);
 
+		list.stream().map(a->a*a).filter(a->a<100).map(a->a*a*a).forEach(System.out::println);
+	}
+
+	void example2(){
+		Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9,10);
+		List list = stream.map(a->a*a).collect(Collectors.toList());
+		Stream<Integer> stream1 = Stream.of(1,2,3,4,5,6,7,8,9,10);
+		System.out.println(list);
+
+		ArrayList<Integer> array = stream1.filter(a->a%2==0).collect(Collectors.toCollection(ArrayList::new));
+		System.out.println(array);
 	}
 }
